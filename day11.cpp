8756d3743d13293d6f16911dde0e89448d4e7624
Nicola <nicolamorelli30008@gmail.com>
2024-12-11 13:28:01 +0100

    riddle11_2

--- day11.cpp
+++ day11.cpp
@@ -4,6 +4,9 @@
 
 
 #include "utils.h"
+#include "omp.h"
+
+#define N_BLINKS 75
 
 void split_rule(vector<size_t> &v) {
     for (int i = 0; i < v.size(); i++) {
@@ -20,6 +23,13 @@ void split_rule(vector<size_t> &v) {
     }
 }
 
+void split_rule_parallel(vector<size_t> &v) {
+
+    for (int i = 0; i < N_BLINKS; ++i) {
+        split_rule(v);
+    }
+}
+
 void riddle11_1(string file_name) {
 
     ifstream is(file_name);
@@ -29,16 +39,46 @@ void riddle11_1(string file_name) {
         is >> tmp;
         v.push_back(tmp);
     }
-    size_t n_blinks = 25;
+    is.close();
+    size_t n_blinks = N_BLINKS;
     for (int i = 0; i < n_blinks; ++i) {
         split_rule(v);
     }
     cout << "The solution of riddle11_1 is: " << v.size() << endl;
 }
 
-int main(void) {
-    riddle11_1("/home/nicola/Desktop/AoC2024/input_files/day11_1.txt");
+void riddle11_2(string file_name) {
 
+    ifstream is(file_name);
+    vector<vector<size_t>> v{};
+    size_t tmp = 0;
+    while (is.good() and is.peek() != EOF) {
+        is >> tmp;
+        v.push_back(vector<size_t>{tmp});
+    }
+    is.close();
+
+#pragma omp parallel for simd
+    for (int i = 0; i < v.size(); ++i)
+        split_rule_parallel(v[i]);
+
+
+    size_t result = 0;
+    for (const auto &item: v) {
+        result += item.size();
+    }
+    cout << "The solution of riddle11_2 is: " << result << endl;
+}
+
+int main(void) {
+    Timer t("general");
+//    t.start("riddle11_1");
+//    riddle11_1("/home/nicola/Desktop/AoC2024/input_files/day11_1.txt");
+//    t.stop();
+
+    t.start("riddle11_2");
+    riddle11_2("/home/nicola/Desktop/AoC2024/input_files/day11_1.txt");
+    t.stop();
 
     return EXIT_SUCCESS;
 }
